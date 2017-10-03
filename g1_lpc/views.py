fcad4f8d7add647a82458da8bbd046a7525b3dda
igor Brito <igorbrytto@gmail.com>
2017-10-02 21:54:56 -0300

    segundo commit

--- g1_lpc/views.py
+++ g1_lpc/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib import admin
+from rest_framework import routers, serializers, viewsets
+from g1_lpc.models import *
+from g1_lpc.serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'g1_lpc',
+    'rest_framework',
 
 ]
 
